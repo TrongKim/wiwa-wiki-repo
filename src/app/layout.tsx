f3183fed331ed6b080ccda88a11496f123a2cc8f
Hakuryuu <dinhhuunghi96@gmail.com>
2025-06-03 19:34:29 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,7 +21,7 @@ export const metadata: Metadata = {
     siteName: "Wuwabeacon",
     images: [
       {
-        url: "https://wuwabeacon.com/banner-guide.jpg",
+        url: "/banner-guide.jpg",
         width: 1200,
         height: 630,
         alt: "Wuwabeacon - Hướng dẫn nhân vật",
