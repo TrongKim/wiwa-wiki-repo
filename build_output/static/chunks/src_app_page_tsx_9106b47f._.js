02f39f6a6059fc0c50a9e526ca6a54a6747732e9
Kai Elwood <kipmap123@gmail.com>
2025-07-07 02:09:10 +0700

    build new

--- build_output/static/chunks/src_app_page_tsx_9106b47f._.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_c694aa30._.js",
-  "static/chunks/src_app_custom_10657e48.css"
-],
-    source: "dynamic"
-});
